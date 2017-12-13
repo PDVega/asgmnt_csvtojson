c56bed1722dfb67ed4bdc341b6b3f3a2a2708979
pdvega <pdvegaa@gmail.com>
2017-12-13 14:01:51 +0700

    correction

--- convert_csv.js
+++ convert_csv.js
@@ -1,10 +1,20 @@
 const csvFile = './customer-data.csv'
+const fs = require('fs')
 const csv = require('csvtojson')
+
+let arr = []
+
 csv()
-.fromFile(csvFile)
-.on('json', jsonObj => {
-  console.log(jsonObj)
-})
-.on('done', err => {
-  console.log('end')
-})
+  .fromFile(csvFile)
+  .on('json', jsonObj => {
+    arr.push(jsonObj)
+  })
+  .on('done', err => {
+    if(err) return process.exit(1)
+    console.log(arr)
+    fs.writeFile('customer-convert.json', JSON.stringify(arr, null, 2), err => {
+      if(err) return process.exit(1)
+      console.log('done')
+      process.exit(0)
+    })
+  })
